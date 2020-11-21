266b7008a001eaa4e3f200e68dd6545e3be3d891
Fedeloscaltro <federico.scaltriti@libero.it>
2020-11-21 16:28:58 +0100

    Web scraping terminato. Sistemato Space.com e i dettagli di ESA

--- TextExtractor.py
+++ TextExtractor.py
@@ -36,28 +36,29 @@ def text_formatting(text):
 
 def esa_extract(soup):
     """Function used to extract the text from ESA's articles
-        @:param soup the HTML parser"""
+        @:param soup the HTML parser
+        @:return text the extracted text of the article"""
 
     for script in soup(["script", "style", "section", "figcaption", "ol"]):  # kill all noising elements
         script.extract()    # rip it out
 
-    for div in soup.findAll('div', attrs={'class': ['article__video', 'meta article__item', 'share']}):
+    text = soup.findAll("div", attrs={"class": "meta article__item"})[0].get_text()  # get the publication's date
+    text = text.split('\n')[1] + '\n'
+
+    for div in soup.findAll('div', attrs={'class': ['article__video',
+                                                    'share']}):     # kill video-related and share contents
         div.extract()
 
-    for div in soup.findAll('div', attrs={'id': ['cookie_loc']}):
-        div.extract()
-
-    for a in soup.findAll('a', attrs={'class': ['flex-prev', 'flex-next', 'flex-active']}):
-        a.extract()
-
-    text = soup.get_text()  # get text
+    for par in soup(["p", "h1", "h2"]):
+        text += par.get_text() + '\n'   # obtain the desired text
 
     return text
 
 
 def bo_extract(soup):
     """Function used to extract the text from Blue Origin's articles
-        @:param soup the HTML parser"""
+        @:param soup the HTML parser
+        @:return text the extracted text of the article"""
 
     for script in soup(["script", "style", "noscript", "footer"]):  # kill all noising elements
         script.extract()  # rip it out
@@ -78,8 +79,22 @@ def bo_extract(soup):
 
 def space_com_extract(soup):
     """Function used to extract the text from NASA's articles
-    @:param soup the HTML parser"""
+    @:param soup the HTML parser
+    @:return text the extracted text of the article"""
 
+    text = ""
+
+    for dell in soup.findAll('div', attrs={'class': ['failuremessage --hide-me', 'successmessage --hide-me',
+                                                     'sc-bdVaJa cAfMzi']}):
+        dell.extract()
+
+    for script in soup(["script", "style", "button", "form", "aside", "footer", "em"]):  # kill all noising elements
+        script.extract()  # rip it out
+
+    for par in soup(["p", "h1", "time"]):
+        text += par.get_text()+'\n'
+
+    return text
 
 """def nasa_extract(soup):
     Function used to extract the text from NASA's articles
@@ -104,16 +119,15 @@ def space_com_extract(soup):
 def main():
     """Function called to start the text-extraction procedure"""
 
-    file_name = "links1.txt"
+    file_name = "links.txt"
 
     with open(file_name, "r") as file:    # opening the file with all the links
         n_links = int(file.readline())   # from which line start to read the file
         lines = file.readlines()[n_links:]
-
     for url in lines:   # dispatcher for the links
         soup = soup_init(url)   # initialize soup with the url
 
-        with open("Articles_Index/article"+str(n_links)+".txt",
+        with open("Articles/"+str(n_links)+".txt",
                   "w", encoding='utf-8') as f_articles:  # creating 1 article per link
 
             if url.find("https://www.esa.int/") != -1:
@@ -123,14 +137,17 @@ def main():
                 text = bo_extract(soup)  # extract the text from a Blue Origin article
 
             else:
-                text = space_com_extract(soup)   # extract the text from a NASA article
+                text = space_com_extract(soup)   # extract the text from a Space.com article
 
             text = text_formatting(text)    # formatting the text
 
             f_articles.write(text)  # writing the text down to the file
 
-        n_links += 1
-
+            n_links += 1
 
 if __name__ == "__main__":
+    import datetime
+    x = datetime.datetime.now()
     main()
+    y = datetime.datetime.now()
+    print("Elapsed time: ", y-x)

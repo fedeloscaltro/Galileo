4e6c6faf2d47cec8cdc53bcbeba8308e8a65589e
lorenzostorchi <lorenzo.storchi99@gmail.com>
2020-11-24 19:07:35 +0100

    Perfezionato il testo degli articoli

--- TextExtractor.py
+++ TextExtractor.py
@@ -2,6 +2,7 @@ from selenium import webdriver as wd
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup as bs
 from selenium.webdriver.support.wait import WebDriverWait
+from datetime import datetime
 
 options = Options()
 options.headless = True  # set the option "headless" for the web driver
@@ -40,17 +41,17 @@ def esa_extract(soup):
         @:return text the extracted text of the article"""
 
     for script in soup(["script", "style", "section", "figcaption", "ol"]):  # kill all noising elements
-        script.extract()    # rip it out
+        script.extract()  # rip it out
 
     text = soup.findAll("div", attrs={"class": "meta article__item"})[0].get_text()  # get the publication's date
     text = text.split('\n')[1] + '\n'
 
     for div in soup.findAll('div', attrs={'class': ['article__video',
-                                                    'share']}):     # kill video-related and share contents
+                                                    'share']}):  # kill video-related and share contents
         div.extract()
 
     for par in soup(["p", "h1", "h2"]):
-        text += par.get_text() + '\n'   # obtain the desired text
+        text += par.get_text() + '\n'  # obtain the desired text
 
     return text
 
@@ -60,11 +61,23 @@ def bo_extract(soup):
         @:param soup the HTML parser
         @:return text the extracted text of the article"""
 
-    for script in soup(["script", "style", "noscript", "footer"]):  # kill all noising elements
+    text = soup.findAll("span", attrs={'class': 'NewsEntry__meta-date'})[0].get_text()  # get the publication's date
+
+    text = str(datetime.strptime(text, '%b %d, %Y'))    # formatting the date
+    text = text[:10]
+    day = text[8:10]
+    month = text[5:7]
+    year = text[:4]
+    text = day + '/' + month + '/' + year + '\n'    # add the date at the beginning of the text
+
+    for script in soup(["script", "style", "noscript", "footer", "header", "title"]):  # deleting all noising elements
         script.extract()  # rip it out
 
     for a in soup.findAll('a', attrs={'class': 'Btn_skipToContent Cta__Dark'}):
         a.extract()
+
+    for p in soup.findAll('p', attrs={'class': 'image-caption'}):
+        p.extract()
 
     for div in soup.findAll('div', attrs={'class': ['NewsEntry__social']}):
         div.extract()
@@ -72,7 +85,10 @@ def bo_extract(soup):
     for sec in soup.findAll('section', attrs={'class': 'LatestNews'}):
         sec.extract()
 
-    text = soup.get_text()  # get text
+    for span in soup.findAll('span', attrs={'class': 'NewsEntry__meta Kicker'}):
+        span.extract()
+
+    text += soup.get_text()  # get text
 
     return text
 
@@ -84,17 +100,26 @@ def space_com_extract(soup):
 
     text = ""
 
+    date = soup.find("time")['datetime'][:10]   # get the publication's date
+    yyyy = date[:4]     # formatting the date
+    mm = date[5:7]
+    dd = date[8:10]
+    date = dd + "/" + mm + "/" + yyyy + "\n"
+
+    text += date    # add the date at the beginning of the text
+
     for dell in soup.findAll('div', attrs={'class': ['failuremessage --hide-me', 'successmessage --hide-me',
-                                                     'sc-bdVaJa cAfMzi']}):
+                                                     'sc-bdVaJa cAfMzi', 'byline-social']}):
         dell.extract()
 
     for script in soup(["script", "style", "button", "form", "aside", "footer", "em"]):  # kill all noising elements
         script.extract()  # rip it out
 
-    for par in soup(["p", "h1", "time"]):
-        text += par.get_text()+'\n'
+    for par in soup(["p", "h1"]):   # find the relevant article parts
+        text += par.get_text() + '\n'
 
     return text
+
 
 """def nasa_extract(soup):
     Function used to extract the text from NASA's articles
@@ -124,6 +149,7 @@ def main():
     with open(file_name, "r") as file:    # opening the file with all the links
         n_links = int(file.readline())   # from which line start to read the file
         lines = file.readlines()[n_links:]
+
     for url in lines:   # dispatcher for the links
         soup = soup_init(url)   # initialize soup with the url
 
@@ -144,6 +170,7 @@ def main():
             f_articles.write(text)  # writing the text down to the file
 
             n_links += 1
+
 
 if __name__ == "__main__":
     main()

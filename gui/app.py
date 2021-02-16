e064818029ce219e83330e29aa0d9461397dc44e
lorenzostorchi <lorenzo.storchi99@gmail.com>
2021-02-16 19:40:33 +0100

    NDCG completato

--- gui/app.py
+++ gui/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from QueryProcesser import processer
+import math
 
 app = Flask(__name__)
 
@@ -24,28 +25,33 @@ def results():
 
     ap = [0 for j in range(11)]
     mean_ap = 0
+    mean_NDCG = 0
     with open("../benchmark/test_queries", "r") as file:
     	n = 1
     	for q in file.readlines():
     		query['text'] = q
     		print(q)
     		articles, dym = processer(query)
-    		test_ap = benchmark(articles, query, n)
+    		test_ap, NDCG = benchmark(articles, query, n)
     		for i in range(11):
     			print(test_ap[i])
     			ap[i] += test_ap[i]
     		mean_ap += sum(test_ap)/10
+    		mean_NDCG += NDCG
+    		print(NDCG)
     		n += 1
 
     n -= 1
+    n_th = 4
     for i in range(11):
-    	ap[i] /= n    
+    	ap[i] /= (n - n_th)    
     print(ap)
 
-    mean_ap /= n
+    mean_ap /= (n - n_th)
     print(mean_ap)
 
-    print(n)
+    mean_NDCG /= (n - n_th)
+    print(mean_NDCG)
 
     return render_template('results.html', context=articles, dym=dym)
 
@@ -60,7 +66,7 @@ def find_nth(haystack, needle, n):
 
 def benchmark(articles, query, i):
 	ap = [0 for j in range(11)]
-
+	DCG_dict = {}
 	with open("../benchmark/relevant_documents", "r") as file:
 		lines = ""
 		for row in file.readlines():
@@ -77,6 +83,33 @@ def benchmark(articles, query, i):
 	recall = 0
 	art_cont = 0
 	rel_cont = 0
+	rank_rel = 6
+	c = 1
+	GT_DCG = 6
+	for doc in relevant_docs:
+		if c > 1:
+			GT_DCG += rank_rel/math.log(c,2)
+		c += 1
+		if rank_rel >= 2:
+			DCG_dict[doc] = rank_rel
+			rank_rel -= 1
+		else:
+			DCG_dict[doc] = rank_rel
+
+	s = 0
+	for i in range(1,11,1):
+		if i>len(DCG_dict):
+			break
+		try:
+			if i == 1:
+				s += DCG_dict[articles[i-1]['path'][:-1]]
+			else:
+				s += DCG_dict[articles[i-1]['path'][:-1]]/math.log(i,2)
+		except KeyError:
+			continue
+
+	NDCG = s/GT_DCG
+
 	for a in articles:
 		art_cont += 1
 		for doc in relevant_docs:
@@ -86,11 +119,7 @@ def benchmark(articles, query, i):
 				precision = (rel_cont/art_cont) * 100
 				ap[recall] += int(precision)
 	
-	return ap
-
-
-# def DCG():
-
+	return ap, NDCG
 
 
 if __name__ == "__main__":
